e2206ae583615aa70b80981252e3ccf60be090d8
shaphan <chaphannzabonimana@gmail.com>
2020-03-19 12:16:45 +0200

    handle edge cases

--- src/sum.js
+++ src/sum.js
@@ -1,5 +1,10 @@
-const sum = (a, b, ...others) =>
-  [a, b, ...others].reduce((total, n) => total + n, 0);
+const sum = (a, b, ...others) => {
+  const nums = [a, b, ...others]
+    .map((n) => parseInt(n, 10))
+    .filter((n) => !Number.isNaN(n));
+
+  return nums.reduce((total, n) => total + n, 0);
+};
 
 export default sum;
 
